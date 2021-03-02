f5e6fae0d95f61dd15afa88b5dfc373b0c3d152a
João G. Saliba <joao_salibaa@hotmail.com>
2021-03-02 14:14:32 -0300

    HTML e JSON response

--- src/net/codejava/ws/HelloResource.java
+++ src/net/codejava/ws/HelloResource.java
@@ -6,12 +6,27 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
  
-@Path("/bonjour")
+@Path("/")
 public class HelloResource {
  
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String direBonjour() {
         return "Bonjour, tout le monde!";
+    }
+    
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/world")
+    public String sayHTMLHello() {
+    	return "<html><title> Hello Rest</title><body>"
+    			+"<h1> Hello World REST! </h1></body></html>";
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/json")
+    public Studante sayHelloJSON() {
+    	return new Studante(123,"Joao", "Brazil");
     }
 }
---
+++ src/net/codejava/ws/Studante.java
@@ -0,0 +1,39 @@
+package net.codejava.ws;
+
+public class Studante {
+	private int id;
+	private String name;
+	private String country;
+	
+	
+	
+	
+	
+	public Studante(int id, String name, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.country = country;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+}
